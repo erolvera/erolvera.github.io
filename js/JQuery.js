d799c6a8fd00941d65b3c0d1d853cde0cf5efbb9
erolvera <emr.olvera@gmail.com>
2017-11-05 09:21:26 -0600

    Update JQuery.js

--- js/JQuery.js
+++ js/JQuery.js
@@ -1,7 +1,7 @@
 //http://www.eslomas.com/2012/02/jquery-para-abrir-enlaces-en-nueva-ventana-sin-target-_blank/
 <script type="text/javascript">
 $(document).ready(function(){
-   $("a.external").live('click', function() {
+   $("a.external").on('click', function() {
       url = $(this).attr("href");
       window.open(url, '_blank');
       return false;
