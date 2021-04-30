5487cd9e0701e0750ac7bade520798a30fabcb37
erolvera <emr.olvera@gmail.com>
2021-04-30 15:19:11 -0500

    Add files via upload

---
+++ tutoriales/TutorialEclipseNeonJavaWindowBuilderVersion2/TutorialDosVersionDos.js
@@ -0,0 +1,266 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 618, y: 622, width: 60, height: 24 }, target: 3 },
+    ],
+    "69": [
+      { type: "gotoframe", rect: { x: 1059, y: 280, width: 60, height: 24 }, target: 70 },
+    ],
+    "115": [
+      { type: "gotoframe", rect: { x: 1044, y: 591, width: 60, height: 24 }, target: 116 },
+    ],
+    "155": [
+      { type: "gotoframe", rect: { x: 1170, y: 570, width: 60, height: 24 }, target: 156 },
+    ],
+    "166": [
+      { type: "gotoframe", rect: { x: 586, y: 36, width: 60, height: 24 }, target: 167 },
+    ],
+    "182": [
+      { type: "gotoframe", rect: { x: 788, y: 342, width: 60, height: 24 }, target: 183 },
+    ],
+    "210": [
+      { type: "gotoframe", rect: { x: 1169, y: 527, width: 60, height: 24 }, target: 211 },
+    ],
+    "243": [
+      { type: "gotoframe", rect: { x: 1267, y: 288, width: 60, height: 24 }, target: 244 },
+    ],
+    "252": [
+      { type: "gotoframe", rect: { x: 1345, y: 206, width: 60, height: 24 }, target: 253 },
+    ],
+    "296": [
+      { type: "gotoframe", rect: { x: 1148, y: 832, width: 60, height: 24 }, target: 297 },
+    ],
+    "313": [
+      { type: "gotoframe", rect: { x: 903, y: 707, width: 60, height: 24 }, target: 314 },
+    ],
+    "335": [
+      { type: "gotoframe", rect: { x: 1267, y: 831, width: 60, height: 24 }, target: 336 },
+    ],
+    "353": [
+      { type: "gotoframe", rect: { x: 824, y: 337, width: 60, height: 24 }, target: 354 },
+    ],
+    "380": [
+      { type: "gotoframe", rect: { x: 729, y: 532, width: 60, height: 24 }, target: 381 },
+    ],
+    "402": [
+      { type: "gotoframe", rect: { x: 873, y: 565, width: 60, height: 24 }, target: 403 },
+    ],
+    "432": [
+      { type: "gotoframe", rect: { x: 793, y: 339, width: 60, height: 24 }, target: 433 },
+    ],
+    "462": [
+      { type: "gotoframe", rect: { x: 1172, y: 535, width: 60, height: 24 }, target: 463 },
+    ],
+    "480": [
+      { type: "gotoframe", rect: { x: 1624, y: 206, width: 60, height: 24 }, target: 481 },
+    ],
+    "526": [
+      { type: "gotoframe", rect: { x: 1232, y: 134, width: 60, height: 24 }, target: 527 },
+    ],
+    "615": [
+      { type: "gotoframe", rect: { x: 1131, y: 831, width: 60, height: 24 }, target: 616 },
+    ],
+    "628": [
+      { type: "gotoframe", rect: { x: 1135, y: 831, width: 60, height: 24 }, target: 629 },
+    ],
+    "634": [
+      { type: "gotoframe", rect: { x: 907, y: 718, width: 60, height: 24 }, target: 635 },
+    ],
+    "659": [
+      { type: "gotoframe", rect: { x: 1259, y: 831, width: 60, height: 24 }, target: 660 },
+    ],
+    "685": [
+      { type: "gotoframe", rect: { x: 807, y: 324, width: 60, height: 24 }, target: 686 },
+    ],
+    "720": [
+      { type: "gotoframe", rect: { x: 733, y: 532, width: 60, height: 24 }, target: 721 },
+    ],
+    "740": [
+      { type: "gotoframe", rect: { x: 744, y: 411, width: 60, height: 24 }, target: 741 },
+    ],
+    "855": [
+      { type: "gotoframe", rect: { x: 1009, y: 557, width: 60, height: 24 }, target: 856 },
+    ],
+    "894": [
+      { type: "gotoframe", rect: { x: 529, y: 76, width: 60, height: 24 }, target: 895 },
+    ],
+    "995": [
+      { type: "gotoframe", rect: { x: 1349, y: 653, width: 60, height: 24 }, target: 996 },
+    ],
+    "1016": [
+      { type: "gotoframe", rect: { x: 949, y: 640, width: 60, height: 24 }, target: 1017 },
+    ],
+    "1023": [
+      { type: "gotoframe", rect: { x: 1450, y: 342, width: 60, height: 24 }, target: 1024 },
+    ],
+    "1076": [
+      { type: "gotoframe", rect: { x: 1538, y: 653, width: 60, height: 24 }, target: 1077 },
+    ],
+    "1100": [
+      { type: "gotoframe", rect: { x: 1198, y: 974, width: 60, height: 24 }, target: 1101 },
+    ],
+    "1135": [
+      { type: "gotoframe", rect: { x: 1369, y: 783, width: 60, height: 24 }, target: 1136 },
+    ],
+    "1165": [
+      { type: "gotoframe", rect: { x: 1321, y: 977, width: 60, height: 24 }, target: 1166 },
+    ],
+    "1231": [
+      { type: "gotoframe", rect: { x: 614, y: 290, width: 60, height: 24 }, target: 1232 },
+    ],
+    "1258": [
+      { type: "gotoframe", rect: { x: 1709, y: 438, width: 60, height: 24 }, target: 1259 },
+    ],
+    "1279": [
+      { type: "gotoframe", rect: { x: 1057, y: 643, width: 60, height: 24 }, target: 1280 },
+    ],
+    "1294": [
+      { type: "gotoframe", rect: { x: 766, y: 301, width: 60, height: 24 }, target: 1295 },
+    ],
+    "1308": [
+      { type: "gotoframe", rect: { x: 644, y: 150, width: 60, height: 24 }, target: 1309 },
+    ],
+    "1324": [
+      { type: "gotoframe", rect: { x: 1091, y: 195, width: 60, height: 24 }, target: 1325 },
+    ],
+    "1335": [
+      { type: "gotoframe", rect: { x: 1663, y: 428, width: 60, height: 24 }, target: 1336 },
+    ],
+    "1382": [
+      { type: "gotoframe", rect: { x: 1071, y: 636, width: 60, height: 24 }, target: 1383 },
+    ],
+    "1392": [
+      { type: "gotoframe", rect: { x: 1594, y: 352, width: 60, height: 24 }, target: 1393 },
+    ],
+    "1410": [
+      { type: "gotoframe", rect: { x: 893, y: 750, width: 60, height: 24 }, target: 1411 },
+    ],
+    "1495": [
+      { type: "gotoframe", rect: { x: 966, y: 749, width: 60, height: 24 }, target: 1496 },
+    ],
+    "1512": [
+      { type: "gotoframe", rect: { x: 1134, y: 395, width: 60, height: 24 }, target: 1513 },
+    ],
+    "1528": [
+      { type: "gotoframe", rect: { x: 1449, y: 562, width: 60, height: 24 }, target: 1529 },
+    ],
+    "1549": [
+      { type: "gotoframe", rect: { x: 1336, y: 813, width: 60, height: 24 }, target: 1550 },
+    ],
+    "1580": [
+      { type: "gotoframe", rect: { x: 922, y: 747, width: 60, height: 24 }, target: 1581 },
+    ],
+    "1587": [
+      { type: "gotoframe", rect: { x: 1361, y: 508, width: 60, height: 24 }, target: 1588 },
+    ],
+    "1625": [
+      { type: "gotoframe", rect: { x: 1621, y: 768, width: 60, height: 24 }, target: 1626 },
+    ],
+    "1695": [
+      { type: "gotoframe", rect: { x: 886, y: 234, width: 60, height: 24 }, target: 1696 },
+    ],
+    "1700": [
+      { type: "gotoframe", rect: { x: 1112, y: 558, width: 60, height: 24 }, target: 1701 },
+    ],
+    "1725": [
+      { type: "gotoframe", rect: { x: 971, y: 576, width: 60, height: 24 }, target: 1726 },
+    ],
+    "1743": [
+      { type: "gotoframe", rect: { x: 1137, y: 395, width: 60, height: 24 }, target: 1744 },
+    ],
+    "1784": [
+      { type: "gotoframe", rect: { x: 1320, y: 395, width: 60, height: 24 }, target: 1785 },
+    ],
+    "1800": [
+      { type: "gotoframe", rect: { x: 1230, y: 326, width: 60, height: 24 }, target: 1801 },
+    ],
+    "1807": [
+      { type: "gotoframe", rect: { x: 1239, y: 612, width: 60, height: 24 }, target: 1808 },
+    ],
+    "1814": [
+      { type: "gotoframe", rect: { x: 1101, y: 622, width: 60, height: 24 }, target: 1815 },
+    ],
+    "1825": [
+      { type: "gotoframe", rect: { x: 1023, y: 583, width: 60, height: 24 }, target: 1826 },
+    ],
+    "1845": [
+      { type: "gotoframe", rect: { x: 929, y: 699, width: 60, height: 24 }, target: 1846 },
+    ],
+    "1878": [
+      { type: "gotoframe", rect: { x: 1050, y: 681, width: 60, height: 24 }, target: 1879 },
+    ],
+    "1886": [
+      { type: "gotoframe", rect: { x: 1258, y: 481, width: 60, height: 24 }, target: 1887 },
+    ],
+    "1936": [
+      { type: "gotoframe", rect: { x: 845, y: 573, width: 60, height: 24 }, target: 1937 },
+    ],
+    "2023": [
+      { type: "gotoframe", rect: { x: 1118, y: 425, width: 60, height: 24 }, target: 2024 },
+    ],
+    "2083": [
+      { type: "gotoframe", rect: { x: 943, y: 592, width: 60, height: 24 }, target: 2084 },
+    ],
+    "2106": [
+      { type: "gotoframe", rect: { x: 559, y: 593, width: 60, height: 24 }, target: 2107 },
+    ],
+    "2152": [
+      { type: "gotoframe", rect: { x: 1131, y: 639, width: 60, height: 24 }, target: 2153 },
+    ],
+    "2219": [
+      { type: "gotoframe", rect: { x: 1072, y: 681, width: 60, height: 24 }, target: 2220 },
+    ],
+    "2266": [
+      { type: "gotoframe", rect: { x: 940, y: 611, width: 60, height: 24 }, target: 2267 },
+    ],
+    "2399": [
+      { type: "gotoframe", rect: { x: 1062, y: 680, width: 60, height: 24 }, target: 2400 },
+    ],
+    "2441": [
+      { type: "gotoframe", rect: { x: 1125, y: 384, width: 60, height: 24 }, target: 2442 },
+    ],
+    "2479": [
+      { type: "gotoframe", rect: { x: 913, y: 611, width: 60, height: 24 }, target: 2480 },
+    ],
+    "2618": [
+      { type: "gotoframe", rect: { x: 1069, y: 679, width: 60, height: 24 }, target: 2619 },
+    ],
+    "2670": [
+      { type: "gotoframe", rect: { x: 276, y: 247, width: 60, height: 24 }, target: 2671 },
+    ],
+    "2713": [
+      { type: "gotoframe", rect: { x: 1219, y: 599, width: 60, height: 24 }, target: 2714 },
+    ],
+    "2727": [
+      { type: "gotoframe", rect: { x: 1538, y: 310, width: 60, height: 24 }, target: 2728 },
+    ],
+    "2740": [
+      { type: "gotoframe", rect: { x: 1824, y: 457, width: 60, height: 24 }, target: 2741 },
+    ],
+    "2779": [
+      { type: "gotoframe", rect: { x: 534, y: 195, width: 60, height: 24 }, target: 2780 },
+    ],
+    "2788": [
+      { type: "gotoframe", rect: { x: 1245, y: 246, width: 60, height: 24 }, target: 2789 },
+    ],
+    "2801": [
+      { type: "gotoframe", rect: { x: 913, y: 411, width: 60, height: 24 }, target: 2802 },
+    ],
+    "2830": [
+      { type: "gotoframe", rect: { x: 748, y: 74, width: 60, height: 24 }, target: 2831 },
+    ],
+    "2835": [
+      { type: "gotoframe", rect: { x: 1132, y: 339, width: 60, height: 24 }, target: 2836 },
+    ],
+    "2873": [
+      { type: "gotoframe", rect: { x: 608, y: 341, width: 60, height: 24 }, target: 2874 },
+    ],
+    "2888": [
+      { type: "gotoframe", rect: { x: 366, y: 595, width: 60, height: 24 }, target: 2 },
+    ],
+  },
+};
